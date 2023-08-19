cc53f6fbf7d882e17b787025245e14a692909ae0
Savita Patil <savitapatil728@gmail.com>
2023-08-19 16:58:10 +0530

    JAVA PROGRAMA

---
+++ LabledLoops/src/LabledLoops/DoWhileLabledLoop.java
@@ -0,0 +1,23 @@
+package LabledLoops;
+
+public class DoWhileLabledLoop {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello ");
+			int i=1;
+			rajani:do
+			{
+				int j=1;
+				tammana:do
+				{
+					System.out.println("kodnest");
+					j++;
+					break;
+				}while(j<=5);
+			System.out.println();
+			i++;
+			}while(i<=5);		
+
+}
+
+}
---
+++ LabledLoops/src/LabledLoops/WhileLabledLoop.java
@@ -0,0 +1,21 @@
+package LabledLoops;
+public class WhileLabledLoop {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello ");
+			int i=1;
+			rajani:while(i<=5)
+			{
+				int j=1;
+				tammana:while(j<=5)
+				{
+					System.out.println("kodnest");
+					j++;
+					break;
+				}
+			System.out.println();
+			i++;
+			}			
+
+}
+	}
